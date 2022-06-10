a01f3192e31c49ad80dc267665136d0e7e1ac442
Aji Bayu Permadi <72933632+MrFunnyide@users.noreply.github.com>
2022-06-10 23:04:41 +0700

    minus session manager

---
+++ app/src/main/java/com/example/m_transaksi/model/Login/Data.java
@@ -0,0 +1,39 @@
+package com.example.m_transaksi.model.Login;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Data extends com.example.m_transaksi.model.Registrasi.Data {
+
+	@SerializedName("id")
+	private String id;
+
+	@SerializedName("no_telp")
+	private String noTelp;
+
+	@SerializedName("nama_admin")
+	private String namaAdmin;
+
+	public void setId(String id){
+		this.id = id;
+	}
+
+	public String getId(){
+		return id;
+	}
+
+	public void setNoTelp(String noTelp){
+		this.noTelp = noTelp;
+	}
+
+	public String getNoTelp(){
+		return noTelp;
+	}
+
+	public void setNamaAdmin(String namaAdmin){
+		this.namaAdmin = namaAdmin;
+	}
+
+	public String getNamaAdmin(){
+		return namaAdmin;
+	}
+}
